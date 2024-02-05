baefcc33a304b650f6c456a669cdfd4161893a07
QinRuixin <1044471292@qq.com>
2024-02-05 19:21:58 +0800

    finish: partial Lab3 page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -449,3 +449,30 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+vmprintd(pagetable_t pagetable, int d){
+  for(int i = 0; i < 512; ++i){
+    pte_t pte = pagetable[i];
+    if((pte & PTE_V)){
+      for(int i = 1; i < d; ++i){
+        printf(" ..");
+      }
+      printf(" ..%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+      if (d <= 2)
+        vmprintd((pagetable_t)PTE2PA(pte), d+1);
+    }
+  }
+}
+
+void
+vmprint(pagetable_t pagetable){
+  printf("page table %p\n", pagetable);
+  for(int i = 0; i < 512; ++i){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      printf(" ..%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+      vmprintd((pagetable_t)PTE2PA(pte), 2);
+    }
+  }
+}
