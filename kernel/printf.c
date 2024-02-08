02ec0d9f054eef32c8fc52f410e2850379a4fe29
QinRuixin <1044471292@qq.com>
2024-02-08 22:38:42 +0800

    finish: part of Lab4 backtrace

--- kernel/printf.c
+++ kernel/printf.c
@@ -133,3 +133,15 @@ printfinit(void)
   initlock(&pr.lock, "pr");
   pr.locking = 1;
 }
+
+void
+backtrace(void)
+{
+  uint64 fp = r_fp();
+  printf("backtrace:\n%p\n", *(uint64*)(fp-8));
+
+  while (PGROUNDDOWN(*(uint64*)(fp-16)) == PGROUNDDOWN(fp)){
+    fp = *(uint64*)(fp-16);
+    printf("%p\n", *(uint64*)(fp-8));
+  }
+}
